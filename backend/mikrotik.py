9bfa81d59ed7836ea3bca06402fc3273f285a9e1
Nekooei Developer <nekooei.developer@gmail.com>
2025-05-14 13:47:48 +0330

    Update mikrotik.py

--- backend/mikrotik.py
+++ backend/mikrotik.py
@@ -1,21 +1,22 @@
-# backend/mikrotik.py
 from librouteros import connect
 import os
 
 def set_route_for_ip(ip, route):
     try:
+        # اتصال به MikroTik API
         api = connect(
             username=os.environ.get("API_USER", "admin"),
             password=os.environ.get("API_PASS", ""),
             host=os.environ.get("MIKROTIK_IP", "172.30.30.254"),
             port=int(os.environ.get("API_PORT", 8728))
         )
+        
         # حذف رول قبلی
         for rule in api.path("ip", "firewall", "mangle").select():
             if rule.get("src-address") == ip:
                 api.path("ip", "firewall", "mangle").remove(id=rule['.id'])
 
-        # ساخت رول جدید
+        # اضافه کردن رول جدید
         api.path("ip", "firewall", "mangle").add(
             chain="prerouting",
             action="mark-routing",
