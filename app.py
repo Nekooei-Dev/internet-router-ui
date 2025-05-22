10031dedec1d7884ef927cf7271297cc03bd9f1d
Nekooei Developer <nekooei.developer@gmail.com>
2025-05-22 23:56:05 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-# app.py
-import os
 from flask import Flask
 from backend.routes.auth import auth_bp
 from backend.routes.admin import admin_bp
 from backend.routes.user import user_bp
 from backend.routes.common import common_bp
 
+import os
+
 app = Flask(__name__)
+
+# تنظیم کلید امنیتی از محیط
 app.secret_key = os.environ.get("SECRET_KEY", "9f7e2c45b6a14d9a8e4d31f0c5b2a7e1")
 
-# Register Blueprints
+# ثبت بلوپرینت‌ها
 app.register_blueprint(auth_bp)
-app.register_blueprint(admin_bp)
-app.register_blueprint(user_bp)
+app.register_blueprint(admin_bp, url_prefix="/admin")
+app.register_blueprint(user_bp, url_prefix="/user")
 app.register_blueprint(common_bp)
 
 if __name__ == "__main__":
